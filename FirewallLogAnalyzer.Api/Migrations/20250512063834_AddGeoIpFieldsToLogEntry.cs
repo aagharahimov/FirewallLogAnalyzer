c407913013c14266a971acc4209e4d76d2ec65df
agasi <agarahimovagasi2@gmail.com>
2025-05-14 16:37:03 +0400

    updated

---
+++ FirewallLogAnalyzer.Api/Migrations/20250512063834_AddGeoIpFieldsToLogEntry.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FirewallLogAnalyzer.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGeoIpFieldsToLogEntry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DestinationGeoCity",
+                table: "LogEntries",
+                type: "TEXT",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DestinationGeoCountry",
+                table: "LogEntries",
+                type: "TEXT",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "DestinationLatitude",
+                table: "LogEntries",
+                type: "REAL",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "DestinationLongitude",
+                table: "LogEntries",
+                type: "REAL",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SourceGeoCity",
+                table: "LogEntries",
+                type: "TEXT",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SourceGeoCountry",
+                table: "LogEntries",
+                type: "TEXT",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "SourceLatitude",
+                table: "LogEntries",
+                type: "REAL",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "SourceLongitude",
+                table: "LogEntries",
+                type: "REAL",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DestinationGeoCity",
+                table: "LogEntries");
+
+            migrationBuilder.DropColumn(
+                name: "DestinationGeoCountry",
+                table: "LogEntries");
+
+            migrationBuilder.DropColumn(
+                name: "DestinationLatitude",
+                table: "LogEntries");
+
+            migrationBuilder.DropColumn(
+                name: "DestinationLongitude",
+                table: "LogEntries");
+
+            migrationBuilder.DropColumn(
+                name: "SourceGeoCity",
+                table: "LogEntries");
+
+            migrationBuilder.DropColumn(
+                name: "SourceGeoCountry",
+                table: "LogEntries");
+
+            migrationBuilder.DropColumn(
+                name: "SourceLatitude",
+                table: "LogEntries");
+
+            migrationBuilder.DropColumn(
+                name: "SourceLongitude",
+                table: "LogEntries");
+        }
+    }
+}
